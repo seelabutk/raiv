8d676e74fefee82936fcfc37668bf3e59588d729
Hunter Price <hunterprice4044@gmail.com>
2023-05-31 11:50:25 -0400

    changed root view to use scoped styles with deep selectors. Added css reset. Fixed action map to not be clipped by initial extension window

--- browser_extension/src/views/RecorderView.vue
+++ browser_extension/src/views/RecorderView.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div id="raiv-container">
     <RecorderControls :store="store" />
     <ConfirmCapture />
   </div>
@@ -10,44 +10,45 @@ import ConfirmCapture from '@/components/ConfirmCapture'
 import RecorderControls from '@/components/RecorderControls'
 import Store from '@/store/store'
 
+import '@/assets/reset.css'
 const store = new Store()
 </script>
 
-<style>
-#raiv {
+<style scoped>
+#raiv-container {
   pointer-events: none; /* See note on next rule. */
   position: absolute;
   right: 1em;
   top: 1em;
   z-index: 10000;
 
-  opacity: 0;
+  opacity: 100;
   transition: opacity 0.45s ease-in-out;
 }
 
-#raiv * {
+#raiv-container:deep(*) {
   color: black;
 }
 
 /* This is a hack. Basically, Vue will mount inside of a div and that
  * div can't be made draggable with our current setup, so the root div
  * will block elements that render underneath the original location. */
-#raiv > div * {
+#raiv-container:deep(div *) {
   pointer-events: auto;
 }
 
-#raiv button {
+#raiv-container:deep(button) {
   cursor: pointer;
   padding: 0.5em;
   height: fit-content;
   width: fit-content;
 }
 
-#raiv button:not(:disabled) {
+#raiv-container:deep(button:not(:disabled)) {
   border: 1px solid black;
 }
 
-#raiv dialog {
+#raiv-container:deep(dialog) {
   border: 1px solid black;
   border-radius: 4px;
   margin: 0;
@@ -57,12 +58,12 @@ const store = new Store()
   width: fit-content;
 }
 
-#raiv input,
-#raiv select {
+#raiv-container:deep(input),
+#raiv-container:deep(select) {
   width: unset;
 }
 
-#raiv .handle {
+#raiv-container:deep(.handle) {
   align-items: center;
   border-bottom: 1px solid black;
   box-sizing: border-box;
@@ -72,13 +73,15 @@ const store = new Store()
   width: 100%;
 }
 
-#raiv .handle .close-btn {
+#raiv-container:deep(.handle .close-btn) {
   align-items: center;
   display: flex;
   margin-right: 0.5em;
   margin-left: auto;
 }
+</style>
 
+<style>
 .raiv-hovered {
   border: 2px solid #0000ff;
 }
