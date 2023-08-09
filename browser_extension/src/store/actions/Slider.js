c90876d80b663eb38696d4af6b94974c7426b8eb
Hunter Price <hunterprice4044@gmail.com>
2023-08-09 15:48:48 -0500

    removed unused commented code

--- browser_extension/src/store/actions/Slider.js
+++ browser_extension/src/store/actions/Slider.js
@@ -35,63 +35,6 @@ export default class Slider extends BaseAction {
     }
   }
 
-  // async capture(port, position, root = false, independentActions = undefined) {
-  //   // A slider will have multiple actions, so we need to store each position
-  //   const positions = []
-
-  //   // Rather than having multiple actions for a single slider action, we
-  //   // will iterate in the capture method
-  //   const tagName = this.target.tagName.toLowerCase()
-  //   const [left, top, right, bottom] = this.boundingBox
-  //   const height = bottom - top
-  //   const width = right - left
-  //   const offset =
-  //     this.sliderOrientation === 'horizontal'
-  //       ? width / this.sliderSteps
-  //       : height / this.sliderSteps
-
-  //   // if the tagName is an input we need to set the value directly
-  //   const deltaValue =
-  //     tagName === 'input' ? this.target.max - this.target.min : 0
-  //   const minValue = Number(this.target.min || 0)
-
-  //   for (let i = 0; i < this.sliderSteps; i++) {
-  //     // Store the position index for the slider step
-  //     positions.push(position)
-
-  //     // Calculate the bounding box for the slider step
-  //     this.sliderBoundingBox = [
-  //       left + (this.sliderOrientation === 'horizontal' ? offset * i : 0),
-  //       top + (this.sliderOrientation === 'vertical' ? offset * i : 0),
-  //       left +
-  //       (this.sliderOrientation === 'horizontal' ? offset * (i + 1) : width),
-  //       top +
-  //       (this.sliderOrientation === 'vertical' ? offset * (i + 1) : height),
-  //     ]
-
-  //     // Calculate the center of the bounding box for the slider step
-  //     this.clickPosition = [
-  //       left +
-  //       (this.sliderOrientation === 'horizontal'
-  //         ? offset * (i + 0.5)
-  //         : width / 2),
-  //       top +
-  //       (this.sliderOrientation === 'vertical'
-  //         ? offset * (i + 0.5)
-  //         : height / 2),
-  //     ]
-
-  //     // Calculate the value for the slider step
-  //     this.sliderValue = minValue + deltaValue * ((i + 0.5) / this.sliderSteps)
-
-  //     // Capture the action
-  //     position = await this._capture(port, position, root, independentActions)
-  //   }
-
-  //   this.position = positions
-  //   return position
-  // }
-
   _revertActionPreChildren() {
     // This is a stub method that should be overridden by subclasses
   }
